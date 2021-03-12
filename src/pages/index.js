3350e59ca6625ed7617dd54dd147161e517afcfb
filippo20u <filippo.20u@gmail.com>
2021-03-12 10:13:38 -0800

    styling working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
 import React from "react"
 
 
-
-export default function About() {
+export default function Home() {
   return (
-    <div>
-      <h1>Dr Jeane Bosch Therapy</h1>
+    <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+     <h1>Dr Jeane Bosch Therapy</h1>
       <p>version 0.1 Gatsby no CMS
         
-      </p>
+   </p>
     </div>
   )
 }

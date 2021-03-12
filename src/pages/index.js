1a77f229bc4e9c16e3a4173818d30fb7137d40be
filippo20u <filippo.20u@gmail.com>
2021-03-12 10:46:39 -0800

    styling working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,22 @@
 import React from "react"
 
+import Header from "../components/header"
+import Footer from "../components/footer"
+
 
 export default function Home() {
   return (
     <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
-     <h1>Dr Jeane Bosch Therapy</h1>
-      <p>version 0.1 Gatsby no CMS
-        
-   </p>
+    
+    <Header headerText="Dr. Jeane Bosch Therapy" /> 
+    {/* <Header/>  */}
+
+    
+     <h2>Netlify</h2>
+      <p>version 0.1 Gatsby no CMS</p>
+      <img src="https://source.unsplash.com/random/" alt="" />
+
+      <Footer footerText="filippo20u 2021" /> 
     </div>
   )
 }
